82afed4e229f773351d739918c8a97e4b9490faf
abhinav13 <kabhinav@gmail.com>
2014-04-01 13:40:54 -0500

    Changed Accessor on CurrentFloor

--- Elevator_Threads/Elevator.cs
+++ Elevator_Threads/Elevator.cs
@@ -15,7 +15,7 @@ namespace Elevator_Threads
         private List<Button> _buttons = null;
 
         public ElevatorEnums.ElevatorDirection Direction { get; set; }
-        public int CurrentFloor { get; private set; }
+        public int CurrentFloor { get;  set; }
 
         //Defensive coding, so you cant make an elvator without floors
         private Elevator()

f807626b114322d76e531d036cccdbdcbba1677f
Kumar Abhinav <kabhinav@gmail.com>
2014-04-10 17:29:42 -0500

    Recursing Solution to find the largest sum from a given list of numbers. Does not follow the yoddle guidelines

--- PythonBinaryTree/PythonBinaryTree.py
+++ PythonBinaryTree/PythonBinaryTree.py
@@ -20,9 +20,10 @@ class Node():
 
 def printTree(node):
 
+    print node
     if(node.left != None):
         printTree(node.left)
-    print node
+   
     if(node.right != None):
         printTree(node.right)
 
@@ -31,7 +32,45 @@ headNode = Node("1")
 leftChild = Node("2")
 rightChild = Node("3")
 
+
+
+
+current_sum=0;
+#print lines
+running_sum=0
+
+def calculatesum(lines,running_sum):
+    i=0;
+    global current_sum
+    if len(lines) != 0:
+
+        for element in lines:
+            print "current element",element  
+            current_numbers = element.split()
+            for n in current_numbers:
+                print "current n", n
+                running_sum = int(running_sum) + int(n)
+                print "running_sum",running_sum
+                #Recurse one level down
+                newline = lines[i+1:]
+                print newline
+                print "Recursion begins"
+                calculatesum(newline,running_sum)
+                running_sum = running_sum - int(n) # reset our total 
+            i=i+1        
+    elif running_sum >= current_sum:
+        print "lines were zero, running sum was greater than current_sum"
+        current_sum = running_sum
+    else:
+        print "running_sum end of recursiong " , running_sum
+
+
+lines = [line.strip() for line in open('sample.txt')]
+
+calculatesum(lines,running_sum)
+print "current_sum", current_sum
+
+
 headNode.addleftChild(leftChild)
 headNode.addRightChild(rightChild)
 
-printTree(headNode)

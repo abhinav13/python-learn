470eb52440827dd199b20295ad1b6d13e592f80e
Kumar Abhinav <kabhinav@gmail.com>
2014-04-16 10:37:45 -0500

    Added bit to make sure the node is not visited earlier

--- PythonBinaryTree/PythonBinaryTree.py
+++ PythonBinaryTree/PythonBinaryTree.py
@@ -1,3 +1,4 @@
+import sys
 
 class Node():
   
@@ -5,72 +6,93 @@ class Node():
         self.cargo = cargo
         self.left = left;
         self.right = right;
-    
+        self.visited = None;
                    
     def __str__(self): 
         return str(self.cargo)            
-                               
+                              
     def addleftChild(self, lchild):
         self.left = lchild
 
-    def addRightChild(self, rchild):
+    def addrightChild(self, rchild):
         self.right = rchild
 
+    def __getitem__(self,item):
+        return self
 
 
 def printTree(node):
 
-    print node
-    if(node.left != None):
+    if(node == None):
+        return
+    if(node.visited != True):
         printTree(node.left)
    
-    if(node.right != None):
+    if(node.visited != True):
         printTree(node.right)
 
-
-headNode = Node("1")
-leftChild = Node("2")
-rightChild = Node("3")
+    print "Node=", node,
+    node.visited = True; 
 
 
+#Read the lines into a list
+
+lines = [line.strip() for line in open(sys.argv[1])]
+print lines
+#Init head of tree = None
+head=None
 
 
-current_sum=0;
-#print lines
-running_sum=0
-
-def calculatesum(lines,running_sum):
-    i=0;
-    global current_sum
-    if len(lines) != 0:
-
-        for element in lines:
-            print "current element",element  
-            current_numbers = element.split()
-            for n in current_numbers:
-                print "current n", n
-                running_sum = int(running_sum) + int(n)
-                print "running_sum",running_sum
-                #Recurse one level down
-                newline = lines[i+1:]
-                print newline
-                print "Recursion begins"
-                calculatesum(newline,running_sum)
-                running_sum = running_sum - int(n) # reset our total 
-            i=i+1        
-    elif running_sum >= current_sum:
-        print "lines were zero, running sum was greater than current_sum"
-        current_sum = running_sum
-    else:
-        print "running_sum end of recursiong " , running_sum
 
 
-lines = [line.strip() for line in open('sample.txt')]
+def createTree(lines):
+        global head
+        line_number=0
+        element_number=0
+        nodeList=[] #This contains parent nodes
+        i=0 #This keeps track of which parent Row are on
+        for line in lines:
+               #see if this is the first line by looking at the length of the line
+               current_numbers = line.split()
+               print "current_numbers", current_numbers
+               if len(current_numbers) == 1:
+                   head = Node(current_numbers[0])
+                   nodeList.insert(line_number,head)
+               else:
+                   #This is the second line, this means we need to go create nodes one by oneand attach them to their parents
+                   j=0  # This keeps track of the numbers that we are currently processing
+                   second_row=[]
+                   for number in current_numbers:
+                       print "j= ",j, "current number = " , number
+                       parentRow = nodeList[i-1] #This is the row of nodes that are the parents
+                       
+                       newNode = Node(number)
+                       if j==0:
+                           #left most child
+                                                                      
+                           print "Inserting Left Most Child which is number ", number, "Into parent Node " , parentRow[j]
+                           parentRow[j].addleftChild(newNode) #Attach as the left child of the first Node parent row
+                       elif j == len(current_numbers)-1: #This is the last node in the current children, so this can only be attached as the right child of the parent node
+                
+                           print "Inserting Right Most Child which is number ", number, "Into parent Node " , parentRow[j-1]
+                           parentRow[j-1].addrightChild(newNode)
+                       else:
+                           
+                           #Attach it as the left child of the Node in the parent list which is one less than the Child's current index
+                         
+                           print "Inserting Right  Child which is number ", number, "Into parent Node " , parentRow[j-1], "And Left Child into Parent ", parentRow[j]
+                           parentRow[j-1].addrightChild(newNode)
+                           #This node the left child of the parent node which has the same index as its own
+                           parentRow[j].addleftChild(newNode)
 
-calculatesum(lines,running_sum)
-print "current_sum", current_sum
+                       second_row.insert(j,newNode) 
+                       j = j +1
+                   #Now insert these parents in the NodeList
+                   nodeList.insert(i,second_row)     
+               i = i +1
 
+#Create the Binary Tree from the File
+createTree(lines)
+printTree(head)
 
-headNode.addleftChild(leftChild)
-headNode.addRightChild(rightChild)
 

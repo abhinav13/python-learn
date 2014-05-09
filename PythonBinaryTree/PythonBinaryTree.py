22c0cb89d3c476fc05588696b3e0ab2f9da52f5f
abhinav <Kabhinav@gmail.com>
2014-05-08 23:37:43 -0500

    Final version with logging and highest sum of nodes

--- PythonBinaryTree/PythonBinaryTree.py
+++ PythonBinaryTree/PythonBinaryTree.py
@@ -1,4 +1,5 @@
 import sys
+import logging
 
 class Node():
   
@@ -7,7 +8,7 @@ class Node():
         self.left = left
         self.right = right
         self.visited = None
-	self.sumbelow = 0
+	self.sumbelow  = 0 
                    
     def __str__(self): 
         return str(self.cargo)            
@@ -32,57 +33,43 @@ def printTree(node):
     if(node.visited != True):
         printTree(node.right)
 
-    print "Node=", node,
+    print "Node=", node
     node.visited = True; 
 
 
 #Read the lines into a list
 
-lines = [line.strip() for line in open(sys.argv[1])]
-print lines
+
 #Init head of tree = None
 head=None
 
 total=0
 prev_total =0
+
 #Calculate largest sum seen so far
 def calsum(node):
 	#recurse through the tree
 	global total
-	#current_num = node.cargo
-	global prev_total
-	if(node == None):
-		#we have reached the end. So now calculate the sum and store it in a list
-		if prev_total == 0:
-			prev_total = total
-		elif prev_total < total:
-			prev_total = total
-		return
-	#Add the node's value to total and recurse left side
-	#now only visit the left tree if we have not calculated the sum below this node
-	if node.sumbelow != 0:
-		#we dont need to traverse down the tree
-		total = total+node.sumbelow 
+	left_total = 0
+	right_total = 0
+	#if node is null, just return
+	if node == None:
+		return 0;
+	#Traverse the left subtree first
+	if node.sumbelow != 0 :
+		return int(node.cargo) + int(node.sumbelow)
+	left_total = calsum(node.left)
+	#now calculate the right total
+	right_total = calsum(node.right)
+	if left_total > right_total:
+		node.sumbelow = int(node.sumbelow) + left_total
 	else:
-		calsum(node.left)
-		total = total+int(node.cargo)
-	#subtract the left child's value from the total and call right child
-	if(node.left != None):
-		total = total - int(node.left.cargo)
-		print "Subtracting left child", node.left.cargo
-		#now calculate the sum of nodes below this node.
-		if int(node.cargo) + int(node.left.cargo) > int(node.sumbelow):
-			node.sumbelow = int(node.cargo) + int(node.left.cargo)
-	calsum(node.right)
-	
-	if(node.right != None):
-		total = total - int(node.right.cargo)
-		print "Subtracting right child",node.right.cargo
-		if int(node.cargo) + int(node.right.cargo) > int(node.sumbelow):
-			node.sumbelow = int(node.cargo) + int(node.right.cargo)
-	#now mark the node as visited
-	return	
-		
+		node.sumbelow = int(node.sumbelow)+ right_total
+	#now calculate the total so far		
+	total = int(node.cargo) + int(node.sumbelow)
+	return total
+
+
 def createTree(lines):
         global head
         line_number=0
@@ -92,7 +79,7 @@ def createTree(lines):
         for line in lines:
                #see if this is the first line by looking at the length of the line
                current_numbers = line.split()
-               print "current_numbers", current_numbers
+               logging.debug( 'current_numbers %s', current_numbers)
                if len(current_numbers) == 1:
                    head = Node(current_numbers[0])
                    nodeList.insert(line_number,head)
@@ -101,24 +88,25 @@ def createTree(lines):
                    j=0  # This keeps track of the numbers that we are currently processing
                    second_row=[]
                    for number in current_numbers:
-                       print "j= ",j, "current number = " , number
+                       logging.debug( 'j= %d current number = %d' ,j, int(number))
                        parentRow = nodeList[i-1] #This is the row of nodes that are the parents
                        
                        newNode = Node(number)
                        if j==0:
                            #left most child
                                                                       
-                           print "Inserting Left Most Child which is number ", number, "Into parent Node " , parentRow[j]
+                           logging.debug('Inserting Left Most Child which is number %d Into parent Node %s' ,int(number), parentRow[j])
+
                            parentRow[j].addleftChild(newNode) #Attach as the left child of the first Node parent row
                        elif j == len(current_numbers)-1: #This is the last node in the current children, so this can only be attached as the right child of the parent node
                 
-                           print "Inserting Right Most Child which is number ", number, "Into parent Node " , parentRow[j-1]
+                           logging.debug('Inserting Right Most Child which is number %d Into parent Node %s' ,int(number), parentRow[j-1])
                            parentRow[j-1].addrightChild(newNode)
                        else:
                            
                            #Attach it as the left child of the Node in the parent list which is one less than the Child's current index
                          
-                           print "Inserting Right  Child which is number ", number, "Into parent Node " , parentRow[j-1], "And Left Child into Parent ", parentRow[j]
+                           logging.debug('Inserting Right  Child which is number %d Into parent Node %s and left subchild %s' , int(number), parentRow[j-1], parentRow[j])
                            parentRow[j-1].addrightChild(newNode)
                            #This node the left child of the parent node which has the same index as its own
                            parentRow[j].addleftChild(newNode)
@@ -129,9 +117,30 @@ def createTree(lines):
                    nodeList.insert(i,second_row)     
                i = i +1
 
+
+if len(sys.argv)  < 2:
+	print "Usage PythonBinaryTree <FileName> optional debug level such as debug,info,warning"
+	sys.exit()	
+#set logging level 
+if len(sys.argv) > 2:
+	loglevel = sys.argv[2]
+	numeric_level = getattr(logging, loglevel.upper(), None)
+	if not isinstance(numeric_level, int):
+    		raise ValueError('Invalid log level: %s' % loglevel)
+	logging.basicConfig(level=numeric_level)
+#Read the lines into a list
+lines = [line.strip() for line in open(sys.argv[1])]
+logging.debug( lines )
+
+
+
+#Init head of tree = None
+head=None
+
+total=0
 #Create the Binary Tree from the File
+
 createTree(lines)
 printTree(head)
 calsum(head)
 print "total", total
-print "prev_total", prev_total

685617162797aab59edb071ecb356bb8e9a024c9
Kumar Abhinav <kabhinav@gmail.com>
2014-08-08 15:23:47 -0500

    Final Answer

--- pythoncode/ProjEuler_Question8.py
+++ pythoncode/ProjEuler_Question8.py
@@ -4,29 +4,25 @@ import sys
 
 def calprod(numarray, start, end):
 	total =1
-	while(start != end):
+	while(start <= end):
 		total = total*int(numarray[start])
 		start = start +1
 	return total
 
 def greaterthan(numarray,start,end, num):
-	print "now scanning",start,"to end", end, "for number", num	
 	while(start <= end):
 		if int(numarray[start]) < int(num):
 			return True
 		else:
-			start = int(start)+1
-	print "exit"
+			start = start+1
 	return False
 
 #This is inefficent as I am reading all the lines at once
 #I can read this line by line and as I read the lines, I can
 # add them to the list of the big number
 
-with open("question8.txt",'r') as f:
+with open("ProjEuler_Question8_input.txt",'r') as f:
 	alllines = f.readlines()
-	#print d
-	#print type(d)
 
 numberlist = []
 
@@ -35,14 +31,13 @@ for line in alllines:
 	for digits in line:
 		if digits.isdigit():
 			numberlist.append(digits)
-print numberlist
+print "input array " , numberlist
 start = 0
-end = 12
+end =12
 max_prod = calprod(numberlist, start,end)
-print "max_prod",max_prod
-frame_start = start
-frame_end = start
-print "len of input array ",len(numberlist)
+frame_start = start+1
+frame_end = end+1
+
 while(frame_end < len(numberlist)):
 	
 
@@ -54,12 +49,12 @@ while(frame_end < len(numberlist)):
 		if(temp > max_prod):
 			max_prod = temp
 			start = frame_start
+			print 'max frame_start', frame_start,'max frame end',frame_end
+			print numberlist[frame_start:frame_end+1]
 			print "max_prod",max_prod
 			end = frame_end
-	else:
-		print "no go"
-	print 'current frame end',frame_end
+	
 	frame_end = frame_end +1
 	frame_start = frame_start +1
 	
-print max_prod	
+print "max_prod of array ", max_prod	

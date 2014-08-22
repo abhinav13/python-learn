adcf039539e8a829547abfc24dce01de064a6090
Kumar Abhinav <kabhinav@gmail.com>
2014-08-22 10:11:12 -0500

    Finished first working draft of pythonsekeleton.py

--- pythoncode/pythonskeleton.py
+++ pythoncode/pythonskeleton.py
@@ -5,7 +5,7 @@ import argparse
 from enum import Enum
 
 
-functionlookup = {'DEBUG': lambda x: logging.debug(x), 'WARN': lambda x : logging.warn(x), 'INFO': lambda x : logging.warn(x) }
+functionlookup = {'DEBUG': lambda x: logging.debug(x), 'WARN': lambda x : logging.warn(x), 'INFO': lambda x : logging.info(x) }
 
 
 class LogLevel(Enum):
@@ -21,7 +21,8 @@ def get_sourcefilename():
 	return filename 
 
 def get_logfilename():
-	return get_sourcefilename + ".log"
+	ret_val = get_sourcefilename()
+	return str(get_sourcefilename()) + str(".log")
 
 
 
@@ -29,32 +30,32 @@ class MyLog:
 	""" Class Init """
 	def __init__(self,filename=None, loglevel=None):
 		if filename == None:
-			filename = get_logfileName()
+			filename = get_logfilename()
 		if loglevel == None:
 			loglevel = 'DEBUG' 
 		elif not isinstance(getattr(logging,loglevel.upper(),None), int):
 			raise ValueError('Invalid Log Level specified :%s' %loglevel)
 
-		self["filename"] = filename
-		self["loglevel"] = loglevel
+		self.filename = filename
+		self.loglevel = loglevel
 		logging.basicConfig(filename = self.filename, filemode='w', format='%(asctime)s %(message)s', 
-				    datefmt='%m/%d/%Y %I:%M:%S %p', level=loglevel.upper())
+				    datefmt='%m/%d/%Y %I:%M:%S %p', level=self.loglevel.upper())
 
 	"""This function writes to a log file """
-	def write(loglevel,message):
-		if self["loglevel"] == loglevel:
+	def write(self,loglevel,message):
 
-		
+		if loglevel.upper() not in functionlookup.keys():
+			raise ValueError('Invalid Log Level specified :%s' %loglevel)
+		else:
+			functionlookup[loglevel.upper()](message)
 
-
-
-def Mylog(input, loglevel):
 
 
 
 def main():
-	inputfile = Create_InputFileName()
-
+	x = MyLog(loglevel="Debug")
+	x.write("Info","This is a default info message")
+	x.write("Debug","This is a debug message")
 
 
 

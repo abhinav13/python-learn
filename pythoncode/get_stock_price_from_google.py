bedcce5ab966ce565ddaf1437aa90c8360a0e7a0
Kumar Abhinav <kabhinav@gmail.com>
2015-03-31 16:34:56 -0500

    Fixed get stock prices to print on the same line along with change in prices

--- pythoncode/get_stock_price_from_google.py
+++ pythoncode/get_stock_price_from_google.py
@@ -1,19 +1,20 @@
 #! /usr/bin/python
 
-
+from __future__ import print_function
 import requests
 from bs4 import BeautifulSoup
 
-print "Enter Stock Symbol"
+print ( "\r Enter Stock Symbol ", end="")
 symbol = raw_input("")
-
 
 if (len(symbol)> 0 ):
     fullpage = requests.get("https://www.google.com/finance?q="+symbol)
     soup = BeautifulSoup(fullpage.text)
     sub = soup.find(id="price-panel")
     #now print the price
-    print sub.div.span.span.text
+    print (sub.div.span.span.text,end="")
+    #sub = soup.find(id="id-price-change nwp goog-inline-block")
+    print (" " + soup.find(class_="ch bld").span.text )
 else:
-    print "Invalid Symbol"
+    print ("Invalid Symbol")
     exit(-1)

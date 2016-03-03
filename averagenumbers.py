9b559a98ae57323e931161ac6ac3bdeb916e2e9a
abhinav <Kabhinav@gmail.com>
2016-03-02 23:40:41 -0600

    mAdded python lru cache files

---
+++ averagenumbers.py
@@ -0,0 +1,15 @@
+def isfloat(value):
+      try:
+        float(value)
+        return True
+      except ValueError:
+        return False
+
+
+
+list=[]
+with open("inputnumbers.txt") as f:
+    for line in f:
+        if isfloat(line):
+            list.append(float(line.strip()))
+print sorted(list) #prints sorted list in ascending order

7d49ed940e6e2489f0edff0130d129e4279b31a0
Kumar Abhinav <kabhinav@gmail.com>
2017-03-18 00:19:16 -0500

    really added the solution to substring problem

--- puzzles/substring_concate_all_words.py
+++ puzzles/substring_concate_all_words.py
@@ -8,38 +8,44 @@
 
 def remove_word(a, word):
     ret_val = []
+    print("removing word" ,word)
     for el in a:
-        if word != a:
+        if word != el:
             ret_val.append(el)
     return ret_val
 
 def does_substring_exist(s, current_index, words):
     length = len(words[0]) 
-    w = s[current_index:length+1]   
+    print ("i = {i}, length = {l} s= {s}".format(i=current_index, l=length, s=s))
+    w = s[current_index:(current_index+length)]   
+    print("now searching for words that start with ",w)
     for el in words:
         if el == w: # found one, so we found one word that matches, now we need to see if the remaining elements are in
                     # remaining string
-        print("Found a potential match")
-        remainder = s[(current_index+len(el)):((len(words)-1)*length)]
-        print("remainder = {r}".format(r=remainder))
-        search_words = remove_word(words, el)
-        found = True
-        for el in search_words:
-            if el not in remainder:
-                print("Did not find {w} in string {r}".format(w=el,r=remainder))
-                found = False
-                break
-        if not found:
-            print("returning false")
-            return False
-        else:
-            print("Returning true")
-            return True
+            print("Found a potential match ", el)
+            begin = current_index + len(el)
+            end = begin+(len(words)-1)*length
+            print ("Begin = {b}, end = {e}".format(b=begin,e=end))
+            remainder = s[begin:end]
+            print("remainder = {r}".format(r=remainder))
+            search_words = remove_word(words, el)
+            print("search for words after match", search_words)
+            found = True
+            for el in search_words:
+                if el not in remainder:
+                    print("Did not find {w} in string {r}".format(w=el,r=remainder))
+                    found = False
+                    break
+            if not found:
+                print("returning false")
+                return False
+            else:
+                print("Returning true")
+                return True
 
 s="barfoothefoobarman" 
 words=["foo", "bar"]
 answer = []
-print("starting")
 for i in range(0,len(s)):
     if does_substring_exist(s, i, words):
         answer.append(i)

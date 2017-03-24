a7fd2737ffb9929ca9c1bb65dee7b3568cf5603e
U-AMERICAS\Kumar_Abhinav <kabhinav@gmail.com>
2017-03-24 10:25:45 -0500

    Added k_pair_smallest_sum.py solution

--- puzzles/k_pair_smallest_sum.py
+++ puzzles/k_pair_smallest_sum.py
@@ -1,9 +1,21 @@
 from collections import defaultdict
 from collections import OrderedDict
+from itertools import product
 
-def create_pairs(array1, array2 ):
 
-    ret_val = [ (x, y) for x in array1 for y in array2 ]
+def create_pairs2(array1, array2):
+    """ This is using the itertools moduels to  give
+        me a cartesian product of the array
+    """
+    array = list()
+    array.append(array1)
+    array.append(array2)
+    array = list(product(*array))
+    return array
+
+
+def create_pairs(array1, array2):
+    ret_val = [(x, y) for x in array1 for y in array2]
     return ret_val
 
 
@@ -21,3 +33,14 @@ h = OrderedDict(sorted(d.items(), key=lambda t: t[0]))
 
 print(d)
 print(h)
+
+# Print K elements from the Ordered dict
+
+j = 0
+k = 3
+for i, el in enumerate(h):
+    for element in h[el]:
+        print(element)
+        j = j+1
+    if j == k:  # We printed the required number of elements
+        break

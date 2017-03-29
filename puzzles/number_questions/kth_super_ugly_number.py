5e7d96bf2c93bb111247b7b54ccc964b66019653
U-AMERICAS\Kumar_Abhinav <kabhinav@gmail.com>
2017-03-29 10:59:35 -0500

    kth_super ugly number solution finished

--- puzzles/number_questions/kth_super_ugly_number.py
+++ puzzles/number_questions/kth_super_ugly_number.py
@@ -6,14 +6,36 @@
 #    (1) 1 is a super ugly number for any given primes.
 #    (2) The given numbers in primes are in ascending order.
 #    (3) 0 < k < = 100, 0 < n < = 106, 0 < primes[i] < 1000.
-
+from collections import defaultdict
 
 def get_primefactors(number):
-    l = []
-    for i in range(1,int(number/2)):
-        prime_factos = [2,3,5,7,11,13,17,19,23,29,31]
+    l = defaultdict(int)
+    for i in range(2,number+1):
         # for each number in primefactor
         # if it divides the number without remainder, keep doing it
         # divide the number, then replace the mantissa with the remainder
         # if the new matissai not divisible by the current number, get new
         # number from the list. Keep doing it till the remainder is 0
+        while number%i ==0:
+            l[str(i)] += 1
+            number = int(number/i)
+            if number == 1:
+                #l[str(1)] = 1
+                return l
+primes = [2,7,13,19]
+ugly_numbers = [1]
+
+#now calculate super ugly prime numbers
+for i in range(2,51):
+    d = get_primefactors(i)
+    ugly = True
+    for j in d.keys():
+        if int(j) not in primes:
+            ugly = False
+    if ugly:
+        ugly_numbers.append(i)
+    if len(ugly_numbers) == 12:
+        break
+
+print(ugly_numbers)
+

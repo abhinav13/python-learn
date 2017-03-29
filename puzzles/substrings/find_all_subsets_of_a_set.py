a4c6fa327dfec48ff3cf48788c84bf84fdf86607
U-AMERICAS\Kumar_Abhinav <kabhinav@gmail.com>
2017-03-29 14:22:05 -0500

    More file movements and cleanup

--- puzzles/substrings/find_all_subsets_of_a_set.py
+++ puzzles/substrings/find_all_subsets_of_a_set.py
@@ -39,7 +39,7 @@ def AllSubsets(array, currentindex) :
         return ret_array
 
 
-l = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+l = [1,2,3] #,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 j = []
 print(AllSubsets(l,0))
 

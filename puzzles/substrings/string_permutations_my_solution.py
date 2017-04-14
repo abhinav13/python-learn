893e59a127965e93e591c5a388168938670fd12d
U-AMERICAS\Kumar_Abhinav <kabhinav@gmail.com>
2017-04-14 12:07:17 -0500

    String permutations, finally I understand, need more practice

---
+++ puzzles/substrings/string_permutations_my_solution.py
@@ -0,0 +1,23 @@
+def swap(array, i, j):
+    c = list(array)
+    temp = c[i]
+    c[i] = c[j]
+    c[j] = temp
+    return ''.join(c)
+
+
+def _recurselistoflists(array, index):
+    print(array)
+    if index == len(array)-1:
+        return
+    # print("index = ", index)
+    for i in range(index, len(array)):
+        for j in range(i+1, len(array)):
+            array = swap(array, i, j)
+            # print("after swaping ", array)
+            _recurselistoflists(array, i+1)
+            # now swap back
+            array = swap(array, j, i)
+
+a = 'ABCD'
+_recurselistoflists(a, 0)

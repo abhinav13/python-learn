2d5cd893922ff1c05d259ed95eb10caf180b45c1
A <kabhinav@gmail.com>
2017-08-31 14:20:16 -0500

    Update config_tempest.sh

--- config_tempest.sh
+++ config_tempest.sh
@@ -5,6 +5,7 @@ mkdir ~/tempest
 cd ~/tempest
 /usr/share/openstack-tempest-*/tools/configure-tempest-directory
 
+FILE_NAME='r14rc'
 os_password=''
 while read line;
 do
@@ -12,7 +13,7 @@ do
   then 
     os_password=$(echo $line | cut -d'=' -f2)
   fi
-done <~/r2rc
+done <$FILE_NAME
 
 tools/config_tempest.py --create --deployer-input ~/tempest-deployerinput.conf service_available.swift False  service_available.sahara False service_available.aodh True object-storage-feature-enabled.discoverability False  network-feature-enabled.ipv6_subnet_attributes False identity.uri http://100.82.35.60:5000/v2.0 identity.admin_username admin  identity.admin_password $os_password identity.admin_tenant_name admin
 
